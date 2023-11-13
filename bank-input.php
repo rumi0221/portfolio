d9ea2177739d137e661f05966c40fa78ef9b3bbe
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-11-13 12:20:35 +0900

    完成

--- bank-input.php
+++ bank-input.php
@@ -3,25 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bank.css">
     <title>bank-input</title>
 </head>
 <body>
     <header>
         <h1>貯金計画</h1>
     </header>
+    <br>
+    <br>
     <form action="bank-output" method="POST">
-        <input type="text" name="goal" placeholder="目標金額を入力してください"/>
+        <div class="text">
+            <input  type="text" name="goal" placeholder="目標金額を入力してください"/>
+        </div>
+        <fieldset class="radio-group">
+            <input type="radio" id="1" name="zutu" value="day" checked>
+            <label class="radio-button" for="1">毎日</label>
+            <input type="radio" id="2" name="zutu" value="week">
+            <label class="radio-button" for="2">毎週</label>
+            <input type="radio" id="3" name="zutu" value="month">
+            <label class="radio-button" for="3">毎月</label>
+        </fieldset>
+        <div class="text">
+            <input type="text" name="kin" placeholder="投入金額を入力してください"/>
+        </div>   
         <br>
-        <input type="radio" name="zutu" value="day">毎日
-        <input type="radio" name="zutu" value="week">毎週
-        <input type="radio" name="zutu" value="month">毎月
+        <p>目標金額まであと…</p>
         <br>
-        <input type="text" name="kin">投入金額を入力してください
-        <br>
-        <input type="submit">
-
-        目標金額まであと…
-
+        <div class="btn">
+            <input type="submit" class="btn1" value="入力">
+        </div>
     </form>
 </body>
 </html>
--- bank-output.php
+++ bank-output.php
@@ -1,42 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bank.css">
+    <title>bank-output</title>
+</head>
+<body>
+    
 <?php
+echo '<header>';
 echo '<h1>貯金計画</h1>';
-echo '<table>';
-echo '<tr>';
-echo '<th>目標金額：</th><td>', $_POST['goal'], '</td>';
-echo '</tr><tr>';
-echo '<th>投入間隔：</th><td>';
-switch($_POST['zutu']){
-    case 'day':
-        echo '毎日';
-        break;
-    case 'week':
-        echo '毎週';
-        break;
-    case 'month':
-        echo '毎月';
-        break;   
+echo '</header>';
+echo '<br><br>';
+if(!preg_match('/^\d+$/', $_POST['goal']) || !preg_match('/^\d+$/', $_POST['kin'])){
+    echo '整数を入力してください。';
+}else{
+    echo '<table>';
+    echo '<tr>';
+    echo '<th>目標金額：</th><td>', $_POST['goal'], '</td>';
+    echo '</tr><tr>';
+    echo '<th>投入間隔：</th><td>';
+    switch($_POST['zutu']){
+        case 'day':
+            echo '毎日';
+            break;
+        case 'week':
+            echo '毎週';
+            break;
+        case 'month':
+            echo '毎月';
+            break;   
+    }
+    echo '</td>';
+    echo '</tr><tr>';
+    echo '<th>投入金額：</th><td>', $_POST['kin'], '</td>';
+    echo '</tr>';
+    echo '</table>';
+    $i = ceil($_POST['goal'] / $_POST['kin']);
+    $nen = 0;
+    echo '<p>';
+    switch($_POST['zutu']){
+        case 'day':
+            echo $i, '日で目標金額達成です！';
+            break;
+        case 'week':
+            echo $i, '週で目標金額です！';
+            break;
+        case 'month' :
+            if($i >= 12){
+                $nen = floor($i / 12);
+                $i = ceil($nen % 12);
+                echo $nen, '年', $i, 'ヵ月で目標金額達成です！';
+            }else{
+                echo $i, 'ヵ月で目標金額達成です！';
+            }
+    }
+    echo '</p>';
 }
-echo '</td>';
-echo '</tr><tr>';
-echo '<th>投入金額：</th><td>', $_POST['kin'], '</td>';
-echo '</tr>';
-echo '</table>';
-echo '<br>';
-$i = ceil($_POST['goal'] / $_POST['kin']);
-$nen = 0;
-switch($_POST['zutu']){
-    case 'day':
-        echo $i, '日で目標金額達成です！';
-        break;
-    case 'week':
-        echo $i, '週で目標金額です！';
-        break;
-    case 'month' :
-        if($i >= 12){
-            $nen = floor($i / 12);
-            $i = ceil($nen % 12);
-            echo $nen, '年', $i, 'ヵ月で目標金額達成です！';
-        }else{
-            echo $i, 'ヵ月で目標金額達成です！';
-        }
-}
+?>
+
+<hr>
+<div class="btn">
+    <a href="bank-input.php" class="btn2">戻る</a>
+</div>
+</body>
+</html>
